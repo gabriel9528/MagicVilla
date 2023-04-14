807680c986bdf3347cc73f2437f4a957aec0da04
Gabriel Fernando <gabrielsxf@gmail.com>
2023-04-14 07:06:07 -0500

    DTO y Mapper

---
+++ MagicVilla_API/Migrations/20230414115722_ActualizarTablaVilla.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActualizarTablaVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Villas",
+                newName: "ImagenUrl");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2023, 4, 14, 6, 57, 21, 928, DateTimeKind.Local).AddTicks(5917), new DateTime(2023, 4, 14, 6, 57, 21, 928, DateTimeKind.Local).AddTicks(5902) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2023, 4, 14, 6, 57, 21, 928, DateTimeKind.Local).AddTicks(5920), new DateTime(2023, 4, 14, 6, 57, 21, 928, DateTimeKind.Local).AddTicks(5919) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImagenUrl",
+                table: "Villas",
+                newName: "ImageUrl");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2023, 4, 13, 7, 6, 58, 972, DateTimeKind.Local).AddTicks(7778), new DateTime(2023, 4, 13, 7, 6, 58, 972, DateTimeKind.Local).AddTicks(7765) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "FechaActualizacion", "FechaCreacion" },
+                values: new object[] { new DateTime(2023, 4, 13, 7, 6, 58, 972, DateTimeKind.Local).AddTicks(7781), new DateTime(2023, 4, 13, 7, 6, 58, 972, DateTimeKind.Local).AddTicks(7780) });
+        }
+    }
+}
--- MagicVilla_API/Modelos/Dto/VillaCreateDto.cs
+++ MagicVilla_API/Modelos/Dto/VillaCreateDto.cs
@@ -1,28 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
-namespace MagicVilla_API.Modelos
+namespace MagicVilla_API.Modelos.Dto
 {
-    public class Villa
+    public class VillaCreateDto
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(30)]
         public string Nombre { get; set; }
         public string Detalle { get; set; }
 
         [Required]
         public double Tarifa { get; set; }
-
         public int Ocupantes { get; set; }
-
         public int MetrosCuadrados { get; set; }
-
         public string ImagenUrl { get; set; }
-
         public string Amenidad { get; set; }
-        public DateTime FechaCreacion { get; set; }
-
-        public DateTime FechaActualizacion { get; set; }
     }
 }
